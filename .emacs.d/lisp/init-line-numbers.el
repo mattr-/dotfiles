aca02c4a217a96b8cd1496dc0feb0dacdec8ef16
Matt Rogers <codemattr@gmail.com>
2017-08-01 21:56:45 -0500

    Enable linum mode globally - with exceptions
    Pull in some code from emacswiki.org to selectively disable linum mode
    for certain major modes, special buffers (ones that start with `*`),
    extremely large buffers (> 30mb), and the minibuffer

--- .emacs.d/lisp/init-line-numbers.el
+++ .emacs.d/lisp/init-line-numbers.el
@@ -1,3 +1,33 @@
+(require 'linum)
+
+(defcustom linum-disabled-modes-list
+  '(eshell-mode
+    wl-summary-mode
+    compilation-mode
+    org-mode
+    dired-mode
+    doc-view-mode
+    image-mode)
+  "* List of modes disabled when global linum mode is no"
+  :type '(repeat (sexp :tag "Major mode"))
+  :tag " Major modes where linum is disabled:"
+  :group 'linum
+  )
+
+(defcustom linum-disable-starred-buffers 't
+  "* Disable buffers that have stars in them like *scratch*"
+  :type 'boolean
+  :group 'linum)
+
+(defun linum-on ()
+  "* When linum is running globally, disable line number in modes defined in `linum-disabled-modes-list'. Changed by linum-off. Also turns off numbering in starred modes like *scratch*"
+
+  (unless (or (minibufferp)
+	      (member major-mode linum-disabled-modes-list)
+	      (string-match "*" (buffer-name))
+	      (> (buffer-size) 3000000)) ;; disable for buffers bigger than 3MB
+    (linum-mode 1)))
+
 
 (unless window-system
   (add-hook 'linum-before-numbering-hook
@@ -14,5 +44,7 @@
 
 (unless window-system
   (setq linum-format 'linum-format-func))
+
+(global-linum-mode 1)
 
 (provide 'init-line-numbers)
