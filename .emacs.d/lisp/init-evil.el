cc97fc9038d76381ddc4ef7c1705495d5eee48b4
Matt Rogers <codemattr@gmail.com>
2017-08-08 08:20:11 -0500

    switch to using spacemacs

--- .emacs.d/lisp/init-evil.el
+++
@@ -1,9 +0,0 @@
-(use-package evil :ensure t)
-(use-package evil-surround :ensure t)
-(use-package evil-numbers :ensure t)
-
-(setq evil-want-C-u-scroll t) ; addicted to C-u scrolling
-
-(evil-mode 1)
-
-(provide 'init-evil)
