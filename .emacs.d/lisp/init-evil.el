82d87c81f2f3aed4941b0144d7931d56db147419
Matt Rogers <codemattr@gmail.com>
2017-08-15 20:35:19 -0500

    really re-enable Ctrl-U scrolling

--- .emacs.d/lisp/init-evil.el
+++ .emacs.d/lisp/init-evil.el
@@ -1,8 +1,9 @@
+(setq evil-want-C-u-scroll t) ; addicted to C-u scrolling
+
 (use-package evil :ensure t)
 (use-package evil-surround :ensure t)
 (use-package evil-numbers :ensure t)
 
-(setq evil-want-C-u-scroll t) ; addicted to C-u scrolling
 
 (evil-mode 1)
 
