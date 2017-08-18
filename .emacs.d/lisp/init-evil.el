6733434db52e82e45a77fa9487ab6d7666009958
Matt Rogers <codemattr@gmail.com>
2017-08-18 08:34:38 -0500

    Update evil configuration
    Add evil-matchit and globally enable it. Also globally enable evil-surround

--- .emacs.d/lisp/init-evil.el
+++ .emacs.d/lisp/init-evil.el
@@ -1,7 +1,16 @@
 (setq evil-want-C-u-scroll t) ; addicted to C-u scrolling
 
 (use-package evil :ensure t)
-(use-package evil-surround :ensure t)
+(use-package evil-surround
+  :ensure t
+  :config
+  (global-evil-surround-mode 1))
+
+(use-package evil-matchit
+  :ensure t
+  :config
+  (global-evil-matchit-mode 1))
+
 (use-package evil-numbers :ensure t)
 
 
