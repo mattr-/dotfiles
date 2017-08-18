8ab24c6fd4686cac68e1bd9c2f0182e14d138c79
Matt Rogers <codemattr@gmail.com>
2017-08-18 15:05:44 -0500

    Update the installed package list
    Not sure why I need this with use-package but I'm tired of seeing this
    diff here.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -62,7 +62,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (rainbow-delimiters markdown-mode evil arjen-grey-theme lua-mode helm use-package))))
+    (magit evil-matchit rainbow-delimiters markdown-mode evil arjen-grey-theme lua-mode helm use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
