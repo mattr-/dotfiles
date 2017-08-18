b0aef4b118731b832c011a1b6e343280f8a2f4ff
Matt Rogers <codemattr@gmail.com>
2017-08-18 15:00:18 -0500

    Update the org mode configuration a bit

--- .emacs.d/lisp/init-org-mode.el
+++ .emacs.d/lisp/init-org-mode.el
@@ -1,4 +1,6 @@
 (use-package org-plus-contrib :ensure t)
+(setq org-directory "~/Dropbox/org")
+(setq org-default-notes-file "~/Dropbox/org/todo.org")
 
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 (setq org-agenda-files '("~/Dropbox/org"))
