876ecded5680c195c60bd8d8ac7e570c5990c0da
Matt Rogers <codemattr@gmail.com>
2017-08-21 09:57:00 -0500

    set up evil org-mode

--- .emacs.d/lisp/init-org-mode.el
+++ .emacs.d/lisp/init-org-mode.el
@@ -1,4 +1,13 @@
 (use-package org-plus-contrib :ensure t)
+(use-package evil-org-mode
+  :ensure t
+  :after org
+  :config
+  (add-hook 'org-mode-book 'evil-org-mode)
+  (add-hook 'evil-org-mode-hook
+	    (lambda()
+	      (evil-org-set-key-theme))))
+
 (setq org-directory "~/Dropbox/org")
 (setq org-default-notes-file "~/Dropbox/org/todo.org")
 
