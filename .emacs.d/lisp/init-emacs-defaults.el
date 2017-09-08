5c6a070b358e437d9f1bd46cacb7c07bb87017d9
Matt Rogers <codemattr@gmail.com>
2017-09-08 11:02:25 -0700

    Remove all the useless emacs ui chrome stuff

--- .emacs.d/lisp/init-emacs-defaults.el
+++ .emacs.d/lisp/init-emacs-defaults.el
@@ -1,6 +1,15 @@
 ;;; Various emacs defaults that I don't want scattered throughout my .emacs file
 
-(menu-bar-mode -1) ;; Turn off the menu bar
+(when (and (fboundp 'tool-bar-mode) (not (eq tool-bar-mode -1)))
+  (tool-bar-mode -1))
+(when (and (fboundp 'menu-bar-mode) (not (eq menu-bar-mode -1)))
+  (menu-bar-mode -1))
+(when (and (fboundp 'scroll-bar-mode) (not (eq scroll-bar-mode -1)))
+  (scroll-bar-mode -1))
+;; tooltips in echo-aera
+(when (and (fboundp 'tooltip-mode) (not (eq tooltip-mode -1)))
+  (tooltip-mode -1))
+
 
 ;; Use 'y' or 'n' instead of 'yes' or 'no'
 (fset 'yes-or-no-p 'y-or-n-p)
