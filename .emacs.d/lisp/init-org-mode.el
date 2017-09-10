d4aec7a34f85da09ff5b24f74eacc77bad7cae2b
Matt Rogers <codemattr@gmail.com>
2017-09-10 08:25:12 -0500

    comment out org-evil. doesn't seem to work

--- .emacs.d/lisp/init-org-mode.el
+++ .emacs.d/lisp/init-org-mode.el
@@ -1,13 +1,13 @@
 (use-package org-plus-contrib :ensure t)
-(use-package evil-org
-  :ensure t
-  :after org
-  :commands (evil-org-mode evil-org-recompute-clocks)
-  :init
-  (add-hook 'org-mode-hook 'evil-org-mode)
-  (add-hook 'evil-org-mode-hook
-	    (lambda()
-	      (evil-org-set-key-theme))))
+; (use-package org-evil
+;   :ensure t
+;   :after org
+;   :commands (evil-org-mode evil-org-recompute-clocks)
+;   :init
+;   (add-hook 'org-mode-hook 'evil-org-mode)
+;   (add-hook 'evil-org-mode-hook
+;         (lambda()
+;           (evil-org-set-key-theme))))
 
 (setq org-directory "~/Dropbox/org")
 (setq org-default-notes-file "~/Dropbox/org/todo.org")
