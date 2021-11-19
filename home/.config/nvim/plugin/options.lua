46942f4cafe73a85158c37097b02f5d19bd56b47
Matt Rogers <codemattr@gmail.com>
2021-11-19 10:54:18 -0600

    Add options from normal vim to neovim
    Reformats things as lua and provides better documentation

---
+++ home/.config/nvim/plugin/options.lua
@@ -0,0 +1,63 @@
+local opt = vim.opt
+
+opt.hidden      = true  -- background buffers without writing them. saves marks/undo as well
+opt.shortmess   = opt.shortmess
+                  + "f" --use "(3 of 5)" instead of "(file 3 of 5)"
+                  + "i" --use "[noeol]" instead of "[Incomplete last line]"
+                  + "l" --use "999L, 888C" instead of "999 lines, 888 characters"
+                  + "m" --use "[+]" instead of "[Modified]"
+                  + "n" --use "[New]" instead of "[New File]"
+                  + "r" --use "[RO]" instead of "[readonly]"
+                  + "w" --use "[w]" instead of "written" for file write message
+                        --and "[a]" instead of "appended" for ':w >> file' command
+                  + "x" --use "[dos]" instead of "[dos format]", "[unix]" instead of
+                        --"[unix format]" and "[mac]" instead of "[mac format]".
+                  + "t" --truncate file message at the start if it is too long to fit
+                        --on the command-line, "<" will appear in the left most column.
+                        --Ignored in Ex mode.
+                  + "T" --truncate other messages in the middle if they are too long to
+                        --fit on the command line.  "..." will appear in the middle.
+                        --Ignored in Ex mode.
+                  + "o" --overwrite message for writing a file with subsequent message
+                        --for reading a file (useful for ":wn" or when 'autowrite' on)
+                  + "O" --message for reading a file overwrites any previous message.
+                        --Also for quickfix message (e.g., ":cn").
+opt.number      = true  -- Absolute line numbers
+opt.ruler       = true  -- line and column number of cursor
+opt.hlsearch    = true  -- Highlight searches
+opt.incsearch   = true  -- Incrementally search
+opt.ignorecase  = true  -- Ignore case when searching...
+opt.smartcase   = true  -- ...unless the query is case sensitive
+opt.cursorline  = true  -- Highlight the current line of the cursor
+opt.report      = 0     -- Always report number of lines changed by ex commands
+opt.wrap        = false -- Don't wrap long lines for display by default.
+opt.modelines   = 5     -- Look for five modelines around the beginning and end of a file
+opt.visualbell  = true  -- Don't beep
+opt.expandtab   = true  -- Spaces by default
+opt.tabstop     = 2     -- Two space indent
+opt.softtabstop = 2
+opt.shiftwidth  = 2
+opt.shiftround  = true  -- Round indent to a multiple of shiftwidth
+opt.breakindent = true  -- Make nice paragraphs out of comments
+opt.timeoutlen  = 1200  -- Give me more time for complex mappings
+opt.ttimeoutlen = 10    -- Make Escape work faster
+opt.backup      = false -- No backups
+opt.swapfile    = false -- No swap files. I too like to live...dangerously.
+opt.list	= true  -- Mark up my buffers
+opt.splitbelow	= true	-- New windows on the bottom
+opt.splitright	= true	-- New windows on the right
+opt.equalalways = false	-- I'll handle the window sizes.
+opt.startofline = false -- Attempt to keep the cursor in the same column
+opt.scrolloff	= 3	-- 3 linues of context when scrolling
+opt.showmatch	= true	-- Show matching pairs
+opt.updatetime  = 100   -- Update faster
+
+
+-- if ((&termencoding ==# 'utf-8' || &encoding ==# 'utf-8') && version >= 700) || has("gui_running")
+--   set listchars=trail:·,precedes:«,extends:»,tab:▸-
+--   set showbreak=↪
+-- else
+--   set listchars=tab:>\ ,trail:-,extends:>,precedes:<
+--   set showbreak='+++ '
+-- endif
+
