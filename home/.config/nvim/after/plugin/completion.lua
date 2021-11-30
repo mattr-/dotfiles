f56d4c7d564fadf5197f69cffbc616d4e6175f17
Matt Rogers <codemattr@gmail.com>
2021-11-30 12:34:58 -0600

    Configure a lua LSP server for neovim

--- home/.config/nvim/after/plugin/completion.lua
+++ home/.config/nvim/after/plugin/completion.lua
@@ -45,42 +45,42 @@ end
 -- -- TODO
 -- -- HTML/CSS/JSON LSP config (via vscode-langservers-extracted)
 
--- -- -- Example custom server
--- -- local sumneko_root_path = vim.fn.getenv 'HOME' .. '/.local/bin/sumneko_lua' -- Change to your sumneko root installation
--- -- local sumneko_binary = sumneko_root_path .. '/bin/Linux/lua-language-server'
+-- -- -- Lua custom server setup
+local sumneko_root_path = vim.fn.getenv 'HOME' .. '/.local/lua-ls' -- Change to your sumneko root installation
+local sumneko_binary = sumneko_root_path .. '/macOS/lua-language-server'
 
--- -- -- Make runtime files discoverable to the server
--- -- local runtime_path = vim.split(package.path, ';')
--- -- table.insert(runtime_path, 'lua/?.lua')
--- -- table.insert(runtime_path, 'lua/?/init.lua')
+-- Make runtime files discoverable to the server
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, 'lua/?.lua')
+table.insert(runtime_path, 'lua/?/init.lua')
 
--- -- require('lspconfig').sumneko_lua.setup {
---   -- cmd = { sumneko_binary, '-E', sumneko_root_path .. '/main.lua' },
---   -- on_attach = on_attach,
---   -- capabilities = capabilities,
---   -- settings = {
---     -- Lua = {
---       -- runtime = {
---         -- -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
---         -- version = 'LuaJIT',
---         -- -- Setup your lua path
---         -- path = runtime_path,
---       -- },
---       -- diagnostics = {
---         -- -- Get the language server to recognize the `vim` global
---         -- globals = { 'vim' },
--- -- --       },
---       -- workspace = {
---         -- Make the server aware of Neovim runtime files
---         -- library = vim.api.nvim_get_runtime_file('', true),
---       -- },
---       -- Do not send telemetry data containing a randomized but unique identifier
---       -- telemetry = {
---         -- enable = false,
---       -- },
---     -- },
---   -- },
--- -- }
+require('lspconfig').sumneko_lua.setup {
+  cmd = { sumneko_binary, '-E', sumneko_root_path .. '/main.lua' },
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = runtime_path,
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { 'vim' },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file('', true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menu,menuone,noselect'
