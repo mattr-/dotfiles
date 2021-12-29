fa0c2fe88c52f7b0098c8a5255afa024a1072138
Matt Rogers <codemattr@gmail.com>
2021-12-29 16:32:46 -0600

    Add an autocommand to run packer sync

--- home/.config/nvim/lua/mattr-/plugins.lua
+++ home/.config/nvim/lua/mattr-/plugins.lua
@@ -1,5 +1,13 @@
 _ = vim.cmd [[packadd packer.nvim]]
 
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
 return require("packer").startup({
   function(use)
     local local_use = function(first, second, opts)
