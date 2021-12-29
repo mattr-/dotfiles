1aecfcb2b0f197c910e4052afb693ebfa693da86
Matt Rogers <codemattr@gmail.com>
2021-12-28 20:41:23 -0600

    Only show icons for the completion menu.
    Having the text as well is too much

--- home/.config/nvim/after/plugin/completion.lua
+++ home/.config/nvim/after/plugin/completion.lua
@@ -91,8 +91,7 @@ cmp.setup {
     fields = { "kind", "abbr", "menu" },
     format = function(entry, vim_item)
       -- Kind icons
-      -- vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-      vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+      vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
       vim_item.menu = ({
         nvim_lsp = "[LSP]",
         luasnip = "[Snippet]",
