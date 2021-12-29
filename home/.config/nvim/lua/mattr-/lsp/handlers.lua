84d6286cb3040e2855fddca43e47502d0622a4ee
Matt Rogers <codemattr@gmail.com>
2021-12-29 11:42:45 -0600

    Use non deprecated method for diagnostics

--- home/.config/nvim/lua/mattr-/lsp/handlers.lua
+++ home/.config/nvim/lua/mattr-/lsp/handlers.lua
@@ -22,7 +22,7 @@ local function lsp_keymaps(bufnr)
     bufnr,
     "n",
     "gl",
-    '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({ border = "rounded" })<CR>',
+    '<cmd>lua vim.diagnostic.open_float(0, { scope = "line", border = "rounded" })<CR>',
     opts)
   vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
 end
