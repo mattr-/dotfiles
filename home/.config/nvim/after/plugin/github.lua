49e2843c86c6c846d40ab18392b4394434ed8b42
Matt Rogers <codemattr@gmail.com>
2021-12-30 15:56:42 -0600

    Add mappings to specific octo.nvim commands
    Specificially, `:Octo issue list` and `:Octo pr list` now have mappings

--- home/.config/nvim/after/plugin/github.lua
+++ home/.config/nvim/after/plugin/github.lua
@@ -1,1 +1,9 @@
 require("octo").setup()
+
+-- The mappings that are set up by octo.nvim are buffer local, so we'll add some global customizations below
+
+-- Add a mapping to list issues in the current repo (letting octo.nvim figure that out for us)
+vim.api.nvim_set_keymap("n", ",ghil", ":Octo issue list<CR>", { noremap = true, silent = true })
+
+-- Add a mapping to list PRs in the current repo (letting octo.nvim figure that out for us)
+vim.api.nvim_set_keymap("n", ",ghpl", ":Octo pr list<CR>", { noremap = true, silent = true })
