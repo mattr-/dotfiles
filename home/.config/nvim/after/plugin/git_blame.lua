d7da183387142dc31f150a91b5df182bd16380c2
Matt Rogers <codemattr@gmail.com>
2022-02-16 17:14:52 -0600

    Ignore many more filetypes for git_blame

--- home/.config/nvim/after/plugin/git_blame.lua
+++ home/.config/nvim/after/plugin/git_blame.lua
@@ -1,2 +1,2 @@
 vim.g.gitblame_date_format = "%r"
-vim.g.gitblame_ignored_filetypes = {'packer', 'TelescopePrompt'}
+vim.g.gitblame_ignored_filetypes = {'packer', 'TelescopePrompt', 'NeogitStatus', 'NeogitPopup', 'Trouble', 'gitcommit'}
