3f612e14e9e62af8890fc1bd44e8fb6a114a800e
Matt Rogers <codemattr@gmail.com>
2022-02-16 17:12:43 -0600

    Use the correct options for customizing markdown
    In regular vim land, you can simply run `setl` with the option
    name. In lua land, you need to use the right type of local option,
    either a buffer local option or a window local option. Update the syntax
    to match that.

--- home/.config/nvim/after/ftplugin/markdown.lua
+++ home/.config/nvim/after/ftplugin/markdown.lua
@@ -1,7 +1,7 @@
-vim.b.textwidth = 0
-vim.b.wrap = true
-vim.b.list = false
-vim.b.foldlevel = 1
-vim.b.foldmethod = "syntax"
+vim.bo.textwidth = 0
+vim.wo.wrap = true
+vim.wo.list = false
+vim.wo.foldlevel = 1
+vim.wo.foldmethod = "syntax"
 
 vim.g.mkdp_markdown_css = string.format("%s/site/markdown_preview.css", vim.fn.stdpath("data"))
