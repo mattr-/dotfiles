9139c3f4f14c3b39c84152e598e73692a4b5753d
Matt Rogers <codemattr@gmail.com>
2022-03-28 10:46:34 -0500

    Remove the mapping to have `Enter` trigger completion
    It was more annoying than it was helpful.

--- home/.config/nvim/after/plugin/completion.lua
+++ home/.config/nvim/after/plugin/completion.lua
@@ -64,7 +64,6 @@ cmp.setup {
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     },
-    ['<CR>'] = cmp.mapping.confirm { select = true },
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
