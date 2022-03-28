025c68be6290f4f001f2781285d922923eb8fc99
Matt Rogers <codemattr@gmail.com>
2022-03-28 13:11:30 -0500

    Adjust to a change in the luasnip api
    You can no longer manipulate the `snippets` variable directly.

--- home/.config/nvim/after/plugin/luasnip.lua
+++ home/.config/nvim/after/plugin/luasnip.lua
@@ -21,8 +21,8 @@ ls.config.setup({
 -- TODO: Attempt to load snippet files dynmaically from ~/.config/nvim/lua/mattr-/snippets/<filetype>.lua
 -- But for now, just hardcode the list.
 
-ls.snippets.markdown = R "mattr-.snippets.markdown"
-ls.snippets.ruby = R "mattr-.snippets.ruby"
+ls.add_snippets('markdown',R("mattr-.snippets.markdown"))
+ls.add_snippets('ruby', R("mattr-.snippets.ruby"))
 
 -- cmp is nice but luasnip still needs its own insert mode mappings
 vim.cmd [[
