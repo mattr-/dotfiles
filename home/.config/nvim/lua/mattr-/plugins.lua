c2eddb5675c944e6af0931a02d80d2d9af839343
Matt Rogers <codemattr@gmail.com>
2022-04-11 16:23:46 -0500

    Use a single border for the packer window

--- home/.config/nvim/lua/mattr-/plugins.lua
+++ home/.config/nvim/lua/mattr-/plugins.lua
@@ -118,7 +118,7 @@ packer.startup({
   config = {
     display = {
       open_fn = function()
-        return require('packer.util').float { border = "rounded" }
+        return require('packer.util').float { border = "single" }
       end,
     }
   }
