c47afa16b5c36f89b10060c5c96f9554760fcff2
Matt Rogers <codemattr@gmail.com>
2022-04-11 19:20:19 -0500

    Don't install the phpdoc treesitter grammar.
    It doesn't install on macOS and I don't do enough development with php
    to worry about it.

--- home/.config/nvim/lua/mattr-/config/treesitter.lua
+++ home/.config/nvim/lua/mattr-/config/treesitter.lua
@@ -1,6 +1,7 @@
 return function()
   require("nvim-treesitter.configs").setup({
     ensure_installed = "all",
+    ignore_install = { "phpdoc" },
     sync_install = false,
     highlight = {
       enable = true,
