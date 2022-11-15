5bb66c13219bce8a2fa207758c8b5350250855aa
Matt Rogers <codemattr@gmail.com>
2022-11-15 10:24:42 -0600

    Remove the lsp config from init.lua
    It's handled via packer's plugin process now.

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -3,7 +3,6 @@ vim.g.start_time = vim.fn.reltime()
 
 require "mattr-.globals"
 require "mattr-.plugins"
-require "mattr-.lsp"
 
 vim.defer_fn(function()
   if (packer_plugins and packer_plugins["which-key.nvim"])
