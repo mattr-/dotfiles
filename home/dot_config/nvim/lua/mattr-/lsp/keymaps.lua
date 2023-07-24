e8d09b4f15908a4c2a1e0465a53623feddf2788f
Matt Rogers <codemattr@gmail.com>
2023-07-24 10:21:28 -0500

    Reintroduce LSP support
     - Adds back LuaSnip and nvim-cmp for snippet and completion support
    respectively.
     - Adds back mason and mason-lspconfig for managing LSP servers.
     - Pull in neoconf and neodev for a better Vim plugin and
    config experience.
     - Steal^WBorrow a bunch of code from LazyVim for setting all of this
       up. A lot of it is the same as what I had before, though the style
       and integration with lazy.nvim makes it a bit different.
     - Switch to using the new LspAttach event for handling things that need
       to happen via `on_attach`. (LspAttach has been there since 0.8.0,
       oops)
     - Limit adding keymaps to when the server supports an operation.
     - Support servers that do dynamic registration.
     - For now, rely on an LSP server's built in formatting.

---
+++ home/dot_config/nvim/lua/mattr-/lsp/keymaps.lua
@@ -0,0 +1,130 @@
+local M = {}
+
+---@type PluginLspKeys
+M._keys = nil
+
+---@return (LazyKeys|{has?:string})[]
+function M.get()
+  local Util = require("mattr-.lsp.util")
+  if not M._keys then
+    ---@class PluginLspKeys
+    -- stylua: ignore
+    M._keys =  {
+      -- { "<leader>cd", vim.diagnostic.open_float, desc = "Line Diagnostics" },
+      -- { "<leader>cl", "<cmd>LspInfo<cr>", desc = "Lsp Info" },
+      { "gd", "<cmd>Telescope lsp_definitions<cr>", desc = "Goto Definition", has = "definition" },
+      { "gr", "<cmd>Telescope lsp_references<cr>", desc = "References" },
+      { "gD", vim.lsp.buf.declaration, desc = "Goto Declaration", has = "declaration" },
+      { "gI", "<cmd>Telescope lsp_implementations<cr>", desc = "Goto Implementation", has = "implementation" },
+      { "gy", "<cmd>Telescope lsp_type_definitions<cr>", desc = "Goto T[y]pe Definition" },
+      { "K", vim.lsp.buf.hover, desc = "Hover" },
+      { "gK", vim.lsp.buf.signature_help, desc = "Signature Help", has = "signatureHelp" },
+      { "<c-k>", vim.lsp.buf.signature_help, mode = "i", desc = "Signature Help", has = "signatureHelp" },
+      { "]d", Util.diagnostic_goto("next"), desc = "Next Diagnostic" },
+      { "[d", Util.diagnostic_goto("previous"), desc = "Prev Diagnostic" },
+      { "]e", Util.diagnostic_goto("next", "ERROR"), desc = "Next Error" },
+      { "[e", Util.diagnostic_goto("previous", "ERROR"), desc = "Prev Error" },
+      { "]w", Util.diagnostic_goto("next", "WARN"), desc = "Next Warning" },
+      { "[w", Util.diagnostic_goto("previous", "WARN"), desc = "Prev Warning" },
+      { "<localleader>f", vim.lsp.buf.format, desc = "Format Document", has = "formatting" },
+      { "<localleader>f", vim.lsp.buf.format, desc = "Format Range", mode = "v", has = "rangeFormatting" },
+      { "<leader>ca", vim.lsp.buf.code_action, desc = "Code Action", mode = { "n", "v" }, has = "codeAction" },
+      {
+        "<leader>cA",
+        function()
+          vim.lsp.buf.code_action({
+            context = {
+              only = {
+                "source",
+              },
+              diagnostics = {},
+            },
+          })
+        end,
+        desc = "Source Action",
+        has = "codeAction",
+      },
+      { "<leader>cr", vim.lsp.buf.rename, desc = "Rename", has = "rename" },
+    }
+
+    -- TODO: Check out the inc-rename.nvim plugin
+    -- if require("mattr-.util.lazy").has("inc-rename.nvim") then
+    --   M._keys[#M._keys + 1] = {
+    --     "<leader>cr",
+    --     function()
+    --       local inc_rename = require("inc_rename")
+    --       return ":" .. inc_rename.config.cmd_name .. " " .. vim.fn.expand("<cword>")
+    --     end,
+    --     expr = true,
+    --     desc = "Rename",
+    --     has = "rename",
+    --   }
+    -- else
+    --   M._keys[#M._keys + 1] = { "<leader>cr", vim.lsp.buf.rename, desc = "Rename", has = "rename" }
+    -- end
+  end
+
+  return M._keys
+end
+
+---@param method string
+function M.has(buffer, method)
+  method = method:find("/") and method or "textDocument/" .. method
+  local clients = vim.lsp.get_active_clients({ bufnr = buffer })
+  for _, client in ipairs(clients) do
+    if client.supports_method(method) then
+      return true
+    end
+  end
+  return false
+end
+
+function M.resolve(buffer)
+  local Keys = require("lazy.core.handler.keys")
+  local keymaps = {} ---@type table<string,LazyKeys|{has?:string}>
+
+  local function add(keymap)
+    local keys = Keys.parse(keymap)
+    if keys[2] == false then
+      keymaps[keys.id] = nil
+    else
+      keymaps[keys.id] = keys
+    end
+  end
+
+  -- add the default keymaps
+  for _, keymap in ipairs(M.get()) do
+    add(keymap)
+  end
+
+  -- check if server config has been attached to the nvim-lspconfig
+  -- options and if there are keymaps specific to that server, add them here
+  local opts = require("mattr-.util.lazy").opts("nvim-lspconfig")
+  local clients = vim.lsp.get_active_clients({ bufnr = buffer })
+  for _, client in ipairs(clients) do
+    local maps = opts.servers[client.name] and opts.servers[client.name].keys or {}
+    for _, keymap in ipairs(maps) do
+      add(keymap)
+    end
+  end
+  return keymaps
+end
+
+function M.on_attach(_client, buffer)
+  local Keys = require("lazy.core.handler.keys")
+  local keymaps = M.resolve(buffer)
+
+  for _, keys in pairs(keymaps) do
+    if not keys.has or M.has(buffer, keys.has) then
+      local opts = Keys.opts(keys)
+      ---@diagnostic disable-next-line: no-unknown
+      opts.has = nil
+      opts.silent = opts.silent ~= false
+      opts.buffer = buffer
+      vim.keymap.set(keys.mode or "n", keys[1], keys[2], opts)
+    end
+  end
+end
+
+
+return M
---
+++ home/dot_config/nvim/lua/mattr-/lsp/util.lua
@@ -0,0 +1,44 @@
+local M = {}
+
+-- on_attach is a function that takes a client and a buffer
+function M.on_attach(on_attach)
+  vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(event)
+      local buffer = event.buf
+      local client = vim.lsp.get_client_by_id(event.data.client_id)
+      on_attach(client, buffer)
+    end,
+  })
+end
+
+function M.diagnostic_goto(direction, severity)
+  local go = direction == "next" and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+  local diagnostic_severity = severity and vim.diagnostic.severity[severity] or nil
+  return function()
+    go({ severity = diagnostic_severity })
+  end
+end
+
+function M.setup_highlight_local_references(client, buffer)
+  -- Highlight references when the cursor stops somewhere
+  if client.server_capabilities.documentHighlightProvider then
+    local augroup = vim.api.nvim_create_augroup("mattr_lsp_document_highlight", { clear = false })
+    vim.api.nvim_clear_autocmds({ buffer = buffer })
+    vim.api.nvim_create_autocmd({ "CursorHold" }, {
+      group = augroup,
+      buffer = buffer,
+      callback = function()
+        vim.lsp.buf.document_highlight()
+      end,
+    })
+    vim.api.nvim_create_autocmd({ "CursorMoved" }, {
+      group = augroup,
+      buffer = buffer,
+      callback = function()
+        vim.lsp.buf.clear_references()
+      end,
+    })
+  end
+end
+
+return M
