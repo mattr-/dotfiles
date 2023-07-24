d7ca79335c224ba3d4367d38af66b676d4361e9f
Matt Rogers <codemattr@gmail.com>
2023-07-24 10:11:05 -0500

    Add a method to get a plugin's options by name
    We'll use this elsewhere as part of doing LSP setup.

--- home/dot_config/nvim/lua/mattr-/util/lazy.lua
+++ home/dot_config/nvim/lua/mattr-/util/lazy.lua
@@ -24,4 +24,14 @@ function M.on_very_lazy(fn)
   })
 end
 
+---@param name string
+function M.opts(name)
+  local plugin = require("lazy.core.config").plugins[name]
+  if not plugin then
+    return {}
+  end
+  local Plugin = require("lazy.core.plugin")
+  return Plugin.values(plugin, "opts", false)
+end
+
 return M
