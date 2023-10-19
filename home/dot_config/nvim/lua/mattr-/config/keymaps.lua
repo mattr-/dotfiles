67cbfe6edd6888185ba88c3d0bfe2c9ad8909755
Matt Rogers <codemattr@gmail.com>
2023-10-19 14:54:08 -0500

    feat(neovim): support for lazy's per-mode keymap disabling
    Taken from LazyVim/LazyVim@0bb6280

--- home/dot_config/nvim/lua/mattr-/config/keymaps.lua
+++ home/dot_config/nvim/lua/mattr-/config/keymaps.lua
@@ -2,14 +2,21 @@
 local function map(mode, lhs, rhs, opts)
   local keys = require("lazy.core.handler").handlers.keys
   ---@cast keys LazyKeysHandler
+  local modes = type(mode) == "string" and { mode } or mode
+
+  ---@param m string
+  modes = vim.tbl_filter(function(m)
+    return not (keys.have and keys:have(lhs, m))
+  end, modes)
+
   -- do not create the keymap if a lazy keys handler exists
-  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+  if #modes > 0 then
     opts = opts or {}
     opts.silent = opts.silent ~= false
     if opts.remap and not vim.g.vscode then
       opts.remap = nil
     end
-    vim.keymap.set(mode, lhs, rhs, opts)
+    vim.keymap.set(modes, lhs, rhs, opts)
   end
 end
 
