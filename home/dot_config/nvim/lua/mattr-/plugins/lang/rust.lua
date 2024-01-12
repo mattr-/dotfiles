0e263835b25bbee70d01461a0e6cf73f7e98aade
Matt Rogers <codemattr@gmail.com>
2024-01-12 14:45:42 -0600

    feat(neovim): Run rust tests via neotest

--- home/dot_config/nvim/lua/mattr-/plugins/lang/rust.lua
+++ home/dot_config/nvim/lua/mattr-/plugins/lang/rust.lua
@@ -77,4 +77,16 @@ return {
       },
     },
   },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "rouge8/neotest-rust",
+    },
+    opts = {
+      adapters = {
+        ["neotest-rust"] = {},
+      },
+    },
+  },
 }
