1907f37e398bf5a27c32ad88ee139aedb1e1f3e9
Matt Rogers <codemattr@gmail.com>
2024-02-21 21:35:03 -0600

    feat(neovim): add naive rubocop detection
    Slightly reorder how things are done as well, since we only do the extra
    logic if there's a rubocop config file in the current directory where we
    launch neovim

--- home/dot_config/nvim/lua/mattr-/features/tools/rubocop.lua
+++ home/dot_config/nvim/lua/mattr-/features/tools/rubocop.lua
@@ -7,21 +7,37 @@ local has_rubocop_file = function()
   )
 end
 
+local rubocop_in_gemfile = function()
+  if vim.fn.filereadable("Gemfile") then
+    local file = io.open("Gemfile")
+    if file then
+      for line in file:lines() do
+        if line:match("rubocop") then
+          file:close()
+          return true
+        end
+      end
+    end
+  end
+end
+
 local rubocop_command = function()
   local cmd = nil
-  -- If there is a binstub, prefer that
-  -- If there is a rubocop yaml file default to bare rubocop
-  -- If there is a Gemfile, look for a bundle binstub and use that or use `bundle exec`
-  if vim.fn.executable("bin/rubocop") == 1 then
-    cmd = { "bin/rubocop" }
-  elseif has_rubocop_file() then
-    cmd = { "rubocop" }
-    if vim.fn.filereadable("Gemfile") == 1 then
+  -- Do nothing if there is not a rubocop file
+  if has_rubocop_file() then
+    -- If there is a binstub, prefer that
+    -- If there is a Gemfile, ensure rubocop is in the gemfile.
+    -- Then look for a bundle binstub and use that or use `bundle exec`
+    if vim.fn.executable("bin/rubocop") == 1 then
+      cmd = { "bin/rubocop" }
+    elseif rubocop_in_gemfile() then
       if vim.fn.executable("bin/bundle") == 1 then
         cmd = { "bin/bundle", "exec", "rubocop" }
       else
         cmd = { "bundle", "exec", "rubocop" }
       end
+    else
+      cmd = { "rubocop" }
     end
   end
 
