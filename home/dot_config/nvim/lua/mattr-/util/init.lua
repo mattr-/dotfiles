182941e8af74c72043b5caf7e72b0bf899175ec6
Matt Rogers <codemattr@gmail.com>
2024-06-13 08:55:41 -0500

    fix(neovim): vim.loop -> vim.uv in a few more places
    I missed catching these in the util module the first time I swept
    through here.

--- home/dot_config/nvim/lua/mattr-/util/init.lua
+++ home/dot_config/nvim/lua/mattr-/util/init.lua
@@ -10,8 +10,8 @@ function M.lazy_notify()
   local orig = vim.notify
   vim.notify = temp
 
-  local timer = vim.loop.new_timer()
-  local check = vim.loop.new_check()
+  local timer = vim.uv.new_timer()
+  local check = vim.uv.new_check()
 
   local replay = function()
     timer:stop()
