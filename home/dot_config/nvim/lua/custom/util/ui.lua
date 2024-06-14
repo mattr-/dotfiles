7af6194cba672e24a9da96d2d48ce628b6229aed
Matt Rogers <codemattr@gmail.com>
2024-06-14 14:40:57 -0500

    feat(neovim): add custom notification wrappers
    `vim.notify` is great! let's wrap it in some custom methods in the ui
    module to add some defaults.
    
    `vim.notify` is backed by the `rcarriga/nvim-notify` in my config and
    nicely passes any extra options on to the backend, which is what makes
    this work so well.
    
    Next up: Replace instances of lazy.nvim's helpers with these ones

--- home/dot_config/nvim/lua/custom/util/ui.lua
+++ home/dot_config/nvim/lua/custom/util/ui.lua
@@ -16,4 +16,49 @@ M.fg = function(group_name)
   return hl_group and { fg = string.format("#%06x", hl_group.fg) } or nil
 end
 
+-- Display an error in the UI. Uses vim.notify under the hood
+--
+---@param title string The title for the notification
+---@param message string The message to display
+---@param opts table Options for customizing the display
+---
+M.error = function(title, message, opts)
+  opts = vim.tbl_extend("force", {
+    title = title,
+    icon = Custom.config.icons.diagnostics.Error
+  }, opts or {})
+
+  vim.notify(message, vim.log.levels.ERROR, opts)
+end
+
+-- Display a warning in the UI. Uses vim.notify under the hood
+--
+---@param title string The title for the notification
+---@param message string The message to display
+---@param opts table Options for customizing the display
+---
+M.warn = function(title, message, opts)
+  opts = vim.tbl_extend("force", {
+    title = title,
+    icon = Custom.config.icons.diagnostics.Warn
+  }, opts or {})
+
+  vim.notify(message, vim.log.levels.WARN, opts)
+end
+
+-- Display a informational message in the UI. Uses vim.notify under the hood
+--
+---@param title string The title for the notification
+---@param message string The message to display
+---@param opts table Options for customizing the display
+---
+M.info = function(title, message, opts)
+  opts = vim.tbl_extend("force", {
+    title = title,
+    icon = Custom.config.icons.diagnostics.Info
+  }, opts or {})
+
+  vim.notify(message, vim.log.levels.INFO, opts)
+end
+
 return M
