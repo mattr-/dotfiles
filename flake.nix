3ce7c50a6100f368a610d8174e37fd26be550de5
Matt Rogers <codemattr@gmail.com>
2024-07-05 14:28:03 -0500

    feat: add the nixos-hardware flake

--- flake.nix
+++ flake.nix
@@ -12,6 +12,9 @@
     # Home manager
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Hardware support
+    hardware.url = "github:NixOS/nixos-hardware";
   };
 
   outputs = {
