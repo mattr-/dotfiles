c0843ce3a9349ddeda745db0f373995090e34e1d
Matt Rogers <codemattr@gmail.com>
2024-07-06 09:43:25 -0500

    feat(slugworth): enable sway so i have a gui

--- hosts/slugworth/default.nix
+++ hosts/slugworth/default.nix
@@ -16,6 +16,9 @@
     hostName = "slugworth";
   };
 
+
+  programs.sway.enable = true;
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
