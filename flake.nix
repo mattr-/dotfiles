7136afdd73b6a9a9a756af20aa86dd587ac5b264
Matt Rogers <codemattr@gmail.com>
2024-07-05 22:30:17 -0500

    feat: enable home-manager for slugworth desktop

--- flake.nix
+++ flake.nix
@@ -72,7 +72,7 @@
     # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
       # FIXME replace with your username@hostname
-      "your-username@your-hostname" = home-manager.lib.homeManagerConfiguration {
+      "mattr-@slugworth" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         modules = [
