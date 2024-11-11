b3d37bfb549e1b2e792bc8955f9d1d6db25de569
Matt Rogers <codemattr@gmail.com>
2024-11-11 17:27:31 -0600

    feat(neovim): add neo-tree for directory navigation
    I mostly want this to replace what I used netrw for.

--- home/dot_config/nvim/lua/custom/plugins/editor.lua
+++ home/dot_config/nvim/lua/custom/plugins/editor.lua
@@ -1,4 +1,50 @@
 return {
+  -- neo-tree to replace most of the things i used to do with netrw
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    cmd = "Neotree",
+    keys = {
+      { "<leader>fe",
+        function()
+          require("neo-tree.command").execute({toggle = true})
+        end,
+        desc = "Explorer NeoTree (root dir)",
+      },
+    },
+    deactivate = function()
+      vim.cmd([[Neotree close]])
+    end,
+    init = function()
+      -- use an autocmd for lazy-loading neo-tree instead of directly requiring it
+      vim.api.nvim_create_autocmd("BufEnter", {
+        group = vim.api.nvim_create_augroup("neotree_start_directory", { clear = true }),
+        desc = "Start neo-tree with a directory",
+        once = true,
+        callback = function()
+          if package.loaded["neo-tree"] then
+            return
+          else
+            local stats = vim.uv.fs_stat(vim.fn.argv(0))
+            if stats and stats.type == "directory" then
+              require("neo-tree")
+            end
+          end
+        end,
+      })
+    end,
+    opts = {
+      sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+      open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "toggleterm", "edgy" },
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = { enabled = true },
+        use_libuv_file_watcher = true,
+      },
+      buffers = {
+        terminals_first = true,
+      },
+    }
+  },
   -- telescope for all the searching
   {
     "nvim-telescope/telescope.nvim",
