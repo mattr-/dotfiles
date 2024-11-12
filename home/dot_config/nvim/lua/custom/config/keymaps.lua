253cb94efa8b55435f51d22c4ab98f6be0411fce
Matt Rogers <codemattr@gmail.com>
2024-11-12 14:42:03 -0600

    refactor(neovim): move the lazy.nvim integrated keymap method to util
    I'm playing with another plugin that takes a keymap function to use and
    want to make sure we don't clobber any of the lazy keymaps, so let's
    factor this function out so we can use it elsewhere.

--- home/dot_config/nvim/lua/custom/config/keymaps.lua
+++ home/dot_config/nvim/lua/custom/config/keymaps.lua
@@ -1,24 +1,6 @@
 -- This file is automatically loaded
-local function map(mode, lhs, rhs, opts)
-  local keys = require("lazy.core.handler").handlers.keys
-  ---@cast keys LazyKeysHandler
-  local modes = type(mode) == "string" and { mode } or mode
 
-  ---@param m string
-  modes = vim.tbl_filter(function(m)
-    return not (keys.have and keys:have(lhs, m))
-  end, modes)
-
-  -- do not create the keymap if a lazy keys handler exists
-  if #modes > 0 then
-    opts = opts or {}
-    opts.silent = opts.silent ~= false
-    if opts.remap and not vim.g.vscode then
-      opts.remap = nil
-    end
-    vim.keymap.set(modes, lhs, rhs, opts)
-  end
-end
+local map = Custom.lazy_integrated_map
 
 -- better up/down
 map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
