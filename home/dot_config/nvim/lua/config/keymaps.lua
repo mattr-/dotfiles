c74e7fa0615c607db34089617480b19c8524c0a2
Matt Rogers <codemattr@gmail.com>
2024-11-13 15:49:30 -0600

    feat(neovim): switch to lazyvim
    bufferline and indent-blankline are disabled.
    The move lines maps have been removed.
    noice has been reconfigured to match my previous configuration.

---
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,11 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+-- Remove the move lines maps
+
+vim.keymap.del("n", "<A-j>")
+vim.keymap.del("n", "<A-k>")
+vim.keymap.del("i", "<A-j>")
+vim.keymap.del("i", "<A-k>")
+vim.keymap.del("v", "<A-k>")
+vim.keymap.del("v", "<A-j>")
