485b6f35f906ea4a1496736cd4ab5f2e3d36b651
Matt Rogers <codemattr@gmail.com>
2024-11-13 15:49:30 -0600

    feat(neovim): switch to lazyvim
    bufferline and indent-blankline are disabled.
    The move lines maps have been removed.
    noice has been reconfigured to match my previous configuration.

---
+++ home/dot_config/nvim/lua/plugins/overrides.lua
@@ -0,0 +1,44 @@
+return {
+  --disable bufferline
+  {
+    "akinsho/bufferline.nvim",
+    enabled = false,
+  },
+  --disable indentline
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    enabled = false,
+  },
+  --reconfig noice
+  {
+    "folke/noice.nvim",
+    opts = {
+      cmdline = {
+        view = "cmdline",
+        format = {
+          -- fix icons with new nerd fonts
+          search_down = { kind = "search", pattern = "^/", icon = "󱁴 ", lang = "regex" },
+          search_up = { kind = "search", pattern = "^%?", icon = "󱁴 ", lang = "regex" },
+        },
+      },
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            any = {
+              { find = "%d+L, %d+B" },
+              { find = "; after #%d+" },
+              { find = "; before #%d+" },
+              { find = "%d+ more line" },
+              { find = "%d+ fewer line" },
+              { find = "%d lines?" },
+              { find = "Already at oldest change" },
+              { find = "^E486" }, -- The couldn't find it error
+            },
+          },
+          view = "mini",
+        },
+      },
+    },
+  },
+}
