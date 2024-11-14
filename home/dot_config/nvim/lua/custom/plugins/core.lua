2e4554331d515b820e0b3d018f3166ef1afd8635
Matt Rogers <codemattr@gmail.com>
2024-11-13 21:22:42 -0600

    feat(neovim): replace nvim-web-devicons with mini.icons
    It emulates nvim-web-devicons and uses the newer nf-md-* icons over the
    nf-dev-* set.

--- home/dot_config/nvim/lua/custom/plugins/core.lua
+++ home/dot_config/nvim/lua/custom/plugins/core.lua
@@ -5,8 +5,19 @@ return {
   -- library used by other plugins
   { "nvim-lua/plenary.nvim", lazy = true },
 
-  -- devicons are used by a bunch of plugins
-  { "nvim-tree/nvim-web-devicons", lazy = true },
+  -- mini.icons for fancy icons
+  -- replaces nvim-web-devicons
+  {
+    "echasnovski/mini.icons",
+    lazy = true,
+    opts = {},
+    init = function()
+      package.preload["nvim-web-devicons"] = function()
+        require("mini.icons").mock_nvim_web_devicons()
+        return package.loaded["nvim-web-devicons"]
+      end
+    end,
+  },
 
   -- nui is used by a bunch of other things
   { "MunifTanjim/nui.nvim", lazy = true },
