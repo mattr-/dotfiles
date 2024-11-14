10c1675fc881ad44c051c62d601a9fe10da3a7c1
Matt Rogers <codemattr@gmail.com>
2024-11-13 21:32:26 -0600

    feat(neovim): improve nvim-treesitter startup performance
    - pull in the query_predicates as an init function - which was stolen
      from LazyVim. thank you folke.
    - lazy load it.
    - update the commands to use to load treesitter

--- home/dot_config/nvim/lua/custom/plugins/treesitter.lua
+++ home/dot_config/nvim/lua/custom/plugins/treesitter.lua
@@ -4,7 +4,17 @@ return {
     version = false, -- treesitter doesn't do releases
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
-    cmd = { "TSUpdateSync" },
+    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+    init = function(plugin)
+      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+      -- no longer trigger the **nvim-treesitter** module to be loaded in time.
+      -- Luckily, the only things that those plugins need are the custom queries, which we make available
+      -- during startup.
+      require("lazy.core.loader").add_to_rtp(plugin)
+      require("nvim-treesitter.query_predicates")
+    end,
+    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
     keys = {
       { "<C-space>", desc = "Increment selection" },
       { "<bs>",    desc = "Decrement selection", mode = "x" },
