8c1551e2339dddc91048caf11c6756017f0f99b4
Matt Rogers <codemattr@gmail.com>
2024-11-13 21:34:52 -0600

    feat(neovim): prevent lualine from flickering at startup
    Also disable the lualine_require stuff (which i thought i had disabled
    before) because it's silly.

--- home/dot_config/nvim/lua/custom/plugins/ui.lua
+++ home/dot_config/nvim/lua/custom/plugins/ui.lua
@@ -48,7 +48,23 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
+    init = function()
+      vim.g.lualine_laststatus = vim.o.laststatus
+      if vim.fn.argc(-1) > 0 then
+        -- set an empty statusline till lualine loads
+        vim.o.statusline = " "
+      else
+        -- hide the statusline on the starter page
+        vim.o.laststatus = 0
+      end
+    end,
     opts = function()
+      -- Disable the lualine_require stuff. It's overcomplicated and unnecessary
+      local lualine_require = require("lualine_require")
+      lualine_require.require = require
+
+      vim.o.laststatus = vim.g.lualine_laststatus
+
       return {
         options = {
           theme = "auto",
