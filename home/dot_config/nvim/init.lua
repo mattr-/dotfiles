3f5c0f2c5f3e5603112d95f0dd93bfaa1bafe00c
Matt Rogers <codemattr@gmail.com>
2024-11-13 21:05:25 -0600

    feat(neovim): stop disabling a few builtin plugins
    matchit, matchparen, and netrwPlugin plugin are no longer disabled

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -25,9 +25,6 @@ require("lazy").setup({
       -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
-        "matchit",
-        "matchparen",
-        "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
