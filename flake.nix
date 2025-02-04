7d5a8e638dfc4547d61d87761e91d08c643dfbcc
Matt Rogers <codemattr@gmail.com>
2025-02-04 11:14:32 -0600

    feat(prodnose): integrate home manager
    convert slugworth's home manager config to prodnose because slugworth is
    the desktop linux machine that I won't be running for the foreseeable
    future.

--- flake.nix
+++ flake.nix
@@ -131,7 +131,7 @@
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
-      "mattr-@slugworth" = home-manager.lib.homeManagerConfiguration {
+      "mattr-@prodnose" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         modules = [
