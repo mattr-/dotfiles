a422ca596aecfb6d72a29398ef0eaf0076c29258
Matt Rogers <codemattr@gmail.com>
2025-03-01 13:44:41 -0600

    feat(slugworth): let disko manage mountpoints

--- hosts/slugworth/hardware-configuration.nix
+++ hosts/slugworth/hardware-configuration.nix
@@ -6,6 +6,7 @@
   lib,
   pkgs,
   modulesPath,
+  inputs,
   ...
 }: {
   imports = [
@@ -19,38 +20,11 @@
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/a932ddf9-924b-4ff9-ab12-d4367c1f11aa";
-    fsType = "btrfs";
-    options = ["compress=zstd" "subvol=root"];
-  };
-
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/a932ddf9-924b-4ff9-ab12-d4367c1f11aa";
-    fsType = "btrfs";
-    options = ["compress=zstd" "subvol=home"];
-  };
-
-  fileSystems."/nix" = {
-    device = "/dev/disk/by-uuid/a932ddf9-924b-4ff9-ab12-d4367c1f11aa";
-    fsType = "btrfs";
-    options = ["compress=zstd" "noatime" "subvol=nix"];
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/12CE-A600";
-    fsType = "vfat";
-    options = ["fmask=0022" "dmask=0022"];
-  };
-
-  swapDevices = [];
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
--- hosts/teevee/hardware-configuration.nix
+++ hosts/teevee/hardware-configuration.nix
@@ -25,9 +25,6 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.eno2.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
