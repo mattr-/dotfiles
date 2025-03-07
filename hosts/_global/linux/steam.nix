7e73bede3540ae90eee2a0c7c3d1ae1b44986ec8
Matt Rogers <codemattr@gmail.com>
2025-03-06 21:29:17 -0600

    feat(slugworth): configure and install steam

--- hosts/_global/linux/steam.nix
+++ hosts/_global/linux/steam.nix
@@ -1,6 +1,13 @@
 {
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
+  programs = {
+    gamescope = {
+      enable = true;
+      capSysNice = true;
+    };
+    steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+      remotePlay.openFirewall = true;
+    };
   };
 }
