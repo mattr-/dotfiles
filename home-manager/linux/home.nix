6195998e55a64e3747e7aa3341b47efe20a62704
Matt Rogers <codemattr@gmail.com>
2025-03-08 13:58:32 -0600

    feat: cargo cult an initial firefox config
    https://discourse.nixos.org/t/combining-best-of-system-firefox-and-home-manager-firefox-settings/37721

--- home-manager/linux/home.nix
+++ home-manager/linux/home.nix
@@ -28,6 +28,38 @@
     ./games
   ];
 
+  programs.firefox = {
+    enable = true;
+    profiles = {
+      default = {
+        id = 0;
+        name = "mattr-";
+        isDefault = true;
+        settings = {
+          # "browser.startup.homepage" = "https://duckduckgo.com";
+          "browser.search.defaultenginename" = "DuckDuckGo";
+          "browser.search.order.1" = "DuckDuckGo";
+
+          "signon.rememberSignons" = false;
+          "widget.use-xdg-desktop-portal.file-picker" = 1;
+          "browser.aboutConfig.showWarning" = false;
+          "browser.compactmode.show" = true;
+          "browser.cache.disk.enable" = false; # Be kind to hard drive
+
+          # Firefox 75+ remembers the last workspace it was opened on as part of its session management.
+          # This is annoying, because I can have a blank workspace, click Firefox from the launcher, and
+          # then have Firefox open on some other workspace.
+          "widget.disable-workspace-management" = true;
+        };
+        search = {
+          force = true;
+          default = "DuckDuckGo";
+          order = [ "DuckDuckGo" "Google" ];
+        };
+      };
+    };
+  };
+
   home = {
     username = "mattr-";
     homeDirectory = "/home/mattr-";
---
+++ hosts/_global/linux/graphical/firefox.nix
@@ -0,0 +1,34 @@
+# built from https://discourse.nixos.org/t/combining-best-of-system-firefox-and-home-manager-firefox-settings/37721
+{ config, pkgs, ... }:
+let
+  lock-false = { Value = false ; Status = "locked"; };
+  lock-true = { Value = true; Status = "locked"; };
+  lock-empty-string = { Value = ""; Status = "locked"; };
+in {
+  programs.firefox = {
+    enable = true;
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      DontCheckDefaultBrowser = true;
+      DisablePocket = true;
+      SearchBar = "unified";
+
+      Preferences = {
+        "extensions.pocket.enabled" = lock-false;
+        "browser.newtabpage.pinned" = lock-empty-string;
+        "browser.topsites.contile.enabled" = lock-false;
+        "browser.newtabpage.activity-stream.showSponsored" = lock-false;
+        "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
+      };
+
+      ExtensionSettings = {
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+        };
+      };
+    };
+  };
+}
