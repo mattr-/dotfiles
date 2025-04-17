d22de0d5100c98e5d852962026b4c2f62c40c587
Matt Rogers <codemattr@gmail.com>
2025-04-17 16:06:24 -0500

    feat: package two San Francisco fonts
    packaging from https://github.com/linuxmobile/kaku

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,4 +2,6 @@
 # You can build them using 'nix build .#example'
 pkgs: {
   # example = pkgs.callPackage ./example { };
+  SF-Pro = pkgs.callPackage ./SF-Pro {inherit (pkgs) stdenv;};
+  SF-Pro-mono = pkgs.callPackage ./SF-Pro-mono {};
 }
