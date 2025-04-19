1b5561845a08b1f78d3aa6280fd6e05fbb73d09e
Matt Rogers <codemattr@gmail.com>
2025-04-19 14:14:22 -0500

    feat: add flake parts for modularization

--- flake.nix
+++ flake.nix
@@ -9,6 +9,15 @@
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    # Flake parts
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    # Flake utils
+    flake-utils.url = "github:numtide/flake-utils";
+
     # Disko for declarative disk partitioning
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
@@ -20,90 +29,86 @@
     hardware.url = "github:NixOS/nixos-hardware";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    nixpkgs-darwin,
-    nix-darwin,
-    home-manager,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-    # Supported systems for your flake packages, shell, etc.
-    systems = [
-      "aarch64-linux"
-      "x86_64-linux"
-      "aarch64-darwin"
-      "x86_64-darwin"
-    ];
-    # This is a function that generates an attribute by calling a function you
-    # pass to it, with each system as an argument
-    forAllSystems = nixpkgs.lib.genAttrs systems;
-  in {
-    # Your custom packages
-    # Accessible through 'nix build', 'nix shell', etc
-    packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
-    # Formatter for your nix files, available through 'nix fmt'
-    # Other options beside 'alejandra' include 'nixpkgs-fmt'
-    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+  outputs = inputs @ { self, nixpkgs, flake-parts, flake-utils, ...}:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      # Define the systems you want to support
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
-    # Your custom packages and modifications, exported as overlays
-    overlays = import ./overlays {inherit inputs;};
-    # Reusable nixos modules you might want to export
-    # These are usually stuff you would upstream into nixpkgs
-    nixosModules = import ./modules/nixos;
-    # Reusable home-manager modules you might want to export
-    # These are usually stuff you would upstream into home-manager
-    homeManagerModules = import ./modules/home-manager;
+      # Use perSystem to define outputs that vary by system
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        # Your custom packages for each system
+        packages = import ./pkgs pkgs;
 
-    # NixOS configuration entrypoint
-    # Available through 'nixos-rebuild --flake .#your-hostname'
-    nixosConfigurations = {
-      prodnose = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-          ./hosts/prodnose
-        ];
+        # Formatter for each system
+        formatter = pkgs.alejandra;
+
+        # You could potentially define devShells here too
+        # devShells.default = pkgs.mkShell { ... };
       };
-      slugworth = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-          ./hosts/slugworth
-          home-manager.nixosModules.home-manager {
-            home-manager.useUserPackages = true;
-            home-manager.users.mattr- = import ./home-manager/linux/home.nix;
-            home-manager.extraSpecialArgs = {inherit inputs outputs;};
-          }
-        ];
-      };
-      teevee = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-          ./hosts/teevee
-        ];
-      };
-      knid = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-          ./hosts/knid
-          home-manager.nixosModules.home-manager {
-            home-manager.useUserPackages = true;
-            home-manager.users.mattr- = import ./home-manager/linux/home.nix;
-            home-manager.extraSpecialArgs = {inherit inputs outputs;};
-          }
-        ];
-      };
-      violet = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-          ./hosts/violet
-          home-manager.nixosModules.home-manager {
-            home-manager.useUserPackages = true;
-            home-manager.users.mattr- = import ./home-manager/linux/home.nix;
-            home-manager.extraSpecialArgs = {inherit inputs outputs;};
-          }
-        ];
+
+      # Define outputs that are the same across all systems
+      flake = {
+        # Your custom packages and modifications, exported as overlays
+        overlays = import ./overlays {inherit inputs;}; # Pass inputs if overlays need them
+
+        # Reusable nixos modules
+        nixosModules = import ./modules/nixos; # Pass inputs if modules need them
+
+        # Reusable home-manager modules
+        homeManagerModules = import ./modules/home-manager; # Pass inputs if modules need them
+
+        # NixOS configurations
+        nixosConfigurations = {
+          prodnose = nixpkgs.lib.nixosSystem {
+            specialArgs = { inherit inputs; } // { outputs = self; }; # Pass self as outputs if needed
+            modules = [
+              ./hosts/prodnose
+            ];
+          };
+          slugworth = nixpkgs.lib.nixosSystem {
+             specialArgs = { inherit inputs; } // { outputs = self; };
+             modules = [
+               ./hosts/slugworth
+               inputs.home-manager.nixosModules.home-manager {
+                 home-manager.useUserPackages = true;
+                 home-manager.users.mattr- = import ./home-manager/linux/home.nix;
+                 # Pass self as outputs if needed by home.nix
+                 home-manager.extraSpecialArgs = { inherit inputs; } // { outputs = self; };
+               }
+             ];
+           };
+          teevee = nixpkgs.lib.nixosSystem {
+            specialArgs = { inherit inputs; } // { outputs = self; };
+            modules = [
+              ./hosts/teevee
+            ];
+          };
+          knid = nixpkgs.lib.nixosSystem {
+             specialArgs = { inherit inputs; } // { outputs = self; };
+             modules = [
+               ./hosts/knid
+               inputs.home-manager.nixosModules.home-manager {
+                 home-manager.useUserPackages = true;
+                 home-manager.users.mattr- = import ./home-manager/linux/home.nix;
+                 home-manager.extraSpecialArgs = { inherit inputs; } // { outputs = self; };
+               }
+             ];
+           };
+          violet = nixpkgs.lib.nixosSystem {
+             specialArgs = { inherit inputs; } // { outputs = self; };
+             modules = [
+               ./hosts/violet
+               inputs.home-manager.nixosModules.home-manager {
+                 home-manager.useUserPackages = true;
+                 home-manager.users.mattr- = import ./home-manager/linux/home.nix;
+                 home-manager.extraSpecialArgs = { inherit inputs; } // { outputs = self; };
+               }
+             ];
+           };
+        };
+        # Potentially add home-manager configurations if not managed through NixOS
+        # homeConfigurations = { ... };
       };
     };
-  };
 }
+
