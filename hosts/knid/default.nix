5516a42da0ba86b2a4b24b2b6631d78adfc12cc9
Matt Rogers <codemattr@gmail.com>
2025-04-25 19:26:02 -0500

    refactor(graphics): move amd config to its own file

--- hosts/knid/default.nix
+++ hosts/knid/default.nix
@@ -11,6 +11,7 @@
     ../../system/nixos/services/flatpak.nix
     ../../system/nixos/services/ssh.nix
     ../../system/nixos/hardware/bluetooth.nix
+    ../../system/nixos/hardware/amd
     ../../system/nixos/software/xdg.nix
     ../../system/nixos/software/fonts.nix
     ../../system/nixos/services/greetd.nix
@@ -25,29 +26,10 @@
 
   programs.niri.enable = true;
 
-
   security = {
     # allow wayland lockers to unlock the screen
     pam.services.hyprlock.text = "auth include login";
   };
-
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-    extraPackages = with pkgs; [
-      libva
-      vaapiVdpau
-      libvdpau-va-gl
-      amdvlk
-      mesa
-    ];
-    extraPackages32 = with pkgs.pkgsi686Linux; [
-      vaapiVdpau
-      libvdpau-va-gl
-      amdvlk
-    ];
-  };
-
 
   system = {
     stateVersion = "24.11";
---
+++ system/nixos/hardware/amd/default.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:
+{
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [
+      libva
+      vaapiVdpau
+      libvdpau-va-gl
+      amdvlk
+      mesa
+    ];
+    extraPackages32 = with pkgs.pkgsi686Linux; [
+      vaapiVdpau
+      libvdpau-va-gl
+      amdvlk
+    ];
+  };
+}
