ba05fcef5d9d827e9e327826798f7eb587b82a71
Matt Rogers <codemattr@gmail.com>
2025-10-03 11:22:35 -0500

    fix(neovim): remove the deprecation warning for crates.nvim
    crates.nvim has an in process lsp server now

--- home/dot_config/nvim/lua/dashvim/plugins/lang/rust.lua
+++ home/dot_config/nvim/lua/dashvim/plugins/lang/rust.lua
@@ -2,25 +2,9 @@ return {
 
   -- Autocompletion for crate names
   {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      {
-        "Saecki/crates.nvim",
-        event = { "BufRead Cargo.toml" },
-        opts = {
-          completion = {
-            cmp = { enabled = true },
-          },
-        },
-      },
-    },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      local cmp = require("cmp")
-      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
-        { name = "crates" },
-      }))
-    end,
+    "Saecki/crates.nvim",
+    event = { "BufRead Cargo.toml" },
+    opts = {},
   },
 
   {
