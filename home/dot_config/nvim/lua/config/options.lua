6373108497468fa729a3b75b7e5065ef676ea098
Matt Rogers <codemattr@gmail.com>
2025-10-03 12:36:57 -0500

    chore: updates

--- home/dot_config/nvim/lua/config/options.lua
+++ home/dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,22 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Disable auto format
+vim.g.autoformat = false
+
+-- Disable Snacks animations
+vim.g.snacks_animate = false
+
+-- Custom options
+local opt = vim.opt
+opt.confirm = false -- I don't really want to see confirm popups
+opt.fillchars = {
+    diff = "╱",
+    msgsep = "‾",
+    foldopen = "▾",
+    foldclose = "▸",
+    foldsep = "│",
+}
+opt.mouse = ""    -- Disable mouse by default
+opt.updatetime = 100   -- Trigger CursorHold faster
