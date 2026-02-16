73e9aa5fc60f2d8437203b7668bbcb1052bf7791
Matt Rogers <codemattr@gmail.com>
2026-02-16 08:39:02 -0600

    refactor: move supported systems into a module

--- modules/systems.nix
+++ modules/systems.nix
@@ -4,6 +4,11 @@
 # This is a flake-parts perSystem configuration.
 { ... }:
 {
+  systems = [
+    "aarch64-darwin"
+    "aarch64-linux"
+    "x86_64-linux"
+  ];
   # Configure the formatter for each system
   perSystem = { pkgs, ... }: {
     formatter = pkgs.nixpkgs-fmt;
