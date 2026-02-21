6e42502aa071c96a010a33c56cee70330c65d31c
Matt Rogers <codemattr@gmail.com>
2026-02-21 14:19:02 -0600

    chore: force treesitter to master by default
    i haven't updated things yet

--- home/dot_config/nvim/lua/dashvim/plugins/treesitter.lua
+++ home/dot_config/nvim/lua/dashvim/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     version = false, -- treesitter doesn't do releases
+    branch = "master",
     -- build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
     lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
