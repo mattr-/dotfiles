a79969b6d6b88094950e305006d695d6b2b87768
Matt Rogers <codemattr@gmail.com>
2026-03-06 11:22:53 -0600

    feat: return to a normal command line experience
    This disables noice as I have been running into increasing friction with it.

--- home/dot_config/nvim/lua/dashvim/plugins/ui.lua
+++ home/dot_config/nvim/lua/dashvim/plugins/ui.lua
@@ -83,18 +83,6 @@ return {
           lualine_x = {
             -- stylua: ignore
             {
-              function() return require("noice").api.status.command.get() end,
-              cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
-              color = DashVim.ui.fg("Special")
-            },
-            -- stylua: ignore
-            {
-              function() return require("noice").api.status.mode.get() end,
-              cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-              color = DashVim.ui.fg("Constant")
-            },
-            -- stylua: ignore
-            {
               function() return "  " .. require("dap").status() end,
               cond = function () return package.loaded["dap"] and require("dap").status() ~= "" end,
               color = DashVim.ui.fg("Debug"),
@@ -117,75 +105,6 @@ return {
         },
         extensions = { "neo-tree", "lazy" },
       }
-    end,
-  },
-
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-      cmdline = {
-        view = "cmdline",
-        ---@type table<string, CmdlineFormat>
-        format = {
-          -- fix icons with new nerd fonts
-          search_down = { kind = "search", pattern = "^/", icon = "󱁴 ", lang = "regex" },
-          search_up = { kind = "search", pattern = "^%?", icon = "󱁴 ", lang = "regex" },
-        },
-      },
-      popupmenu = {
-        enabled = false,
-      },
-      lsp = {
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
-      },
-      routes = {
-        {
-          filter = {
-            event = "msg_show",
-            any = {
-              { find = "%d+L, %d+B" },
-              { find = "; after #%d+" },
-              { find = "; before #%d+" },
-              { find = "%d+ more line" },
-              { find = "%d+ fewer line" },
-              { find = "%d lines?" },
-              { find = "Already at oldest change" },
-              { find = "^E486" }, -- The couldn't find it error
-            },
-          },
-          view = "mini",
-        },
-      },
-      presets = {
-        bottom_search = true,
-        command_palette = true,
-        long_message_to_split = true,
-        inc_rename = true,
-      },
-    },
-    -- stylua: ignore
-    keys = {
-      { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
-      { "<leader>vnl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
-      { "<leader>vnh", function() require("noice").cmd("history") end, desc = "Noice History" },
-      { "<leader>vna", function() require("noice").cmd("all") end, desc = "Noice All" },
-      { "<leader>vnd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
-      { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll forward", mode = {"i", "n", "s"} },
-      { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll backward", mode = {"i", "n", "s"}},
-    },
-    config = function(_, opts)
-      -- HACK: noice shows messages from before it was enabled,
-      -- but this is not ideal when Lazy is installing plugins,
-      -- so clear the messages in this case.
-      if vim.o.filetype == "lazy" then
-        vim.cmd([[messages clear]])
-      end
-      require("noice").setup(opts)
     end,
   },
 
