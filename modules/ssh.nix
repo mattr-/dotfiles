594e14ddb89161d250406e92aee92d2bff296e55
Matt Rogers <codemattr@gmail.com>
2026-03-25 15:48:16 -0500

    feat: user ssh configuration

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -11,4 +11,24 @@
 
     programs.ssh.startAgent = true;
   };
+
+  flake.modules.homeManager.ssh = {
+    programs.ssh = {
+      enable = true;
+      enableDefaultConfig = false;
+      matchBlocks."*" = {
+        forwardAgent = false;
+        addKeysToAgent = "no";
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
+      };
+      extraConfig = "Include one_password.conf";
+    };
+  };
 }
