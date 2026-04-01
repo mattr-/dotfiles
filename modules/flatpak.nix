a76f5a8088bb0153157a5b35f1fa11307225a347
Matt Rogers <codemattr@gmail.com>
2026-04-01 13:35:27 -0500

    feat: install MS Edge for XBox Cloud Gaming

--- modules/flatpak.nix
+++ modules/flatpak.nix
@@ -5,7 +5,17 @@
 
     services.flatpak = {
       enable = true;
-      packages = [ "org.signal.Signal" ];
+      packages = [
+        "org.signal.Signal"
+        "com.microsoft.Edge"
+      ];
+      overrides = {
+        "com.microsoft.Edge".Context = {
+          filesystems = [
+            "/run/udev:ro"
+          ];
+        };
+      };
     };
   };
 }
