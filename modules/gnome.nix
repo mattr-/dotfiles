6a5e0eede39432e37b27be6af901faf9ee3982be
Matt Rogers <codemattr@gmail.com>
2026-04-01 13:38:53 -0500

    feat: replace GDM with SDDM
    and install a fancy theme to go with it

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -2,7 +2,6 @@
 {
   flake.modules.nixos.gnome = { pkgs, ... }: {
     services.desktopManager.gnome.enable = true;
-    services.displayManager.gdm.enable = true;
 
     environment.systemPackages = with pkgs; [
       gnomeExtensions.appindicator
