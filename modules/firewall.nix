38c7826668373cfc6716af0daca5607e82b96bdc
Matt Rogers <codemattr@gmail.com>
2026-04-22 14:38:35 -0500

    chore: configure sunshine's ports explicitly
    i'm seeing strange disconnects after the switch to nftables, so let's
    see if an explicit set of port configurations improves the situation.

--- modules/firewall.nix
+++ modules/firewall.nix
@@ -5,7 +5,8 @@
 
     networking.firewall = {
       enable = true;
-      allowedTCPPorts = [ 22 ];
+      allowedTCPPorts = [ 22 47984 47989 48010 ];
+      allowedUDPPorts = [ 47998 47999 48000 48002 48010 ];
     };
   };
 }
