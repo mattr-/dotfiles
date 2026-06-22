dfa05bf4c953d43b3c9b5875d8a2338265dc3128
Matt Rogers <codemattr@gmail.com>
2026-06-22 14:08:32 -0500

    feat: attempt to move from nix to lix
    Reverting commit 4fcb9b2a308c3672addad96d0fedc1e0dd0554da to try lix
    again.

--- modules/nix.nix
+++ modules/nix.nix
@@ -2,10 +2,18 @@
 let
   # Shared nix settings used by both NixOS and nix-darwin
   sharedNixConfig = { pkgs, lib, ... }: {
-    nix.package = pkgs.nixVersions.latest;
+    # Use Lix as the Nix implementation
+    nix.package = pkgs.lixPackageSets.stable.lix;
 
     nix.settings = {
+      # Enable flakes and the nix command
       experimental-features = [ "nix-command" "flakes" ];
+
+      # Lix binary cache
+      extra-substituters = [ "https://cache.lix.systems" ];
+      extra-trusted-public-keys = [
+        "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
+      ];
 
       # Allow the primary user to manage the nix store
       trusted-users = [ "root" "@wheel" "mattr-" ];
