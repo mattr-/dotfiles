7a75516bd7c59f7f8df70a770dbcc0c410e62888
Matt Rogers <codemattr@gmail.com>
2026-06-26 15:23:10 -0500

    feat(darwin): specify a default stateVersion

--- modules/state-version.nix
+++ modules/state-version.nix
@@ -4,6 +4,10 @@
     system.stateVersion = lib.mkDefault "24.05";
   };
 
+  flake.modules.darwin.stateVersion = {
+    system.stateVersion = lib.mkDefault 7;
+  };
+
   flake.modules.homeManager.stateVersion = {
     home.stateVersion = lib.mkDefault "24.05";
   };
