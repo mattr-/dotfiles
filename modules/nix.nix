fb6282bf1434c6a42a704a3e1f0a72c5d742dd72
Matt Rogers <codemattr@gmail.com>
2026-06-26 15:23:21 -0500

    refactor: shrink the nix module
    Less comments because I know what all the shared configuration does

--- modules/nix.nix
+++ modules/nix.nix
@@ -2,36 +2,29 @@
 let
   # Shared nix settings used by both NixOS and nix-darwin
   sharedNixConfig = { pkgs, lib, ... }: {
-    # Use Lix as the Nix implementation
-    nix.package = pkgs.lixPackageSets.stable.lix;
+    nix = {
+      package = pkgs.lixPackageSets.stable.lix;
 
-    nix.settings = {
-      # Enable flakes and the nix command
-      experimental-features = [ "nix-command" "flakes" ];
+      settings = {
+        experimental-features = [ "nix-command" "flakes" ];
 
-      # Lix binary cache
-      extra-substituters = [ "https://cache.lix.systems" ];
-      extra-trusted-public-keys = [
-        "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
-      ];
+        # Lix binary cache
+        extra-substituters = [ "https://cache.lix.systems" ];
+        extra-trusted-public-keys = [
+          "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
+        ];
 
-      # Allow the primary user to manage the nix store
-      trusted-users = [ "root" "@wheel" "mattr-" ];
+        trusted-users = [ "root" "@wheel" "mattr-" ];
+        auto-optimise-store = lib.mkDefault true;
+        warn-dirty = false;
+        flake-registry = "";
+      };
 
-      # Optimize store automatically
-      auto-optimise-store = lib.mkDefault true;
-
-      # Don't warn about dirty git trees
-      warn-dirty = false;
-
-      # Disable global registry
-      flake-registry = "";
-    };
-
-    # Periodic garbage collection
-    nix.gc = {
-      automatic = true;
-      options = "--delete-older-than 30d";
+      # Periodic garbage collection
+      gc = {
+        automatic = true;
+        options = "--delete-older-than 30d";
+      };
     };
   };
 in
