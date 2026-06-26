fb40f0bc72bee277fab9e68daba86dbf3bae2f05
Matt Rogers <codemattr@gmail.com>
2026-06-26 16:10:30 -0500

    feat(darwin): specify a configurationRevision

--- modules/state-version.nix
+++ modules/state-version.nix
@@ -6,6 +6,7 @@
 
   flake.modules.darwin.stateVersion = {
     system.stateVersion = lib.mkDefault 7;
+    system.configurationRevision = outputs.self.rev or outputs.self.dirtyRev or null;
   };
 
   flake.modules.homeManager.stateVersion = {
