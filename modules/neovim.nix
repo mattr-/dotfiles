53a6cf2cc09812522e190104e898ae20ea89412e
Matt Rogers <codemattr@gmail.com>
2026-06-27 15:15:57 -0500

    feat(neovim): imperatively manage neovim

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -1,14 +1,17 @@
 { ... }:
 {
   flake.modules.homeManager.neovim = { pkgs, ... }: {
-    programs.neovim = {
-      enable = true;
-      defaultEditor = true;
-      vimAlias = true;
-      viAlias = true;
-      withRuby = false;
-      withPython3 = false;
-      withPerl = false;
-    };
+    # programs.neovim = {
+    #   enable = true;
+    #   defaultEditor = true;
+    #   vimAlias = true;
+    #   viAlias = true;
+    #   withRuby = false;
+    #   withPython3 = false;
+    #   withPerl = false;
+    # };
+    home.packages = with pkgs; [
+      neovim-unwrapped
+    ];
   };
 }
