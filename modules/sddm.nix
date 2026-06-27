4f12fadc9b0c00b4450dd786a919b17b0c11ff0d
Matt Rogers <codemattr@gmail.com>
2026-06-27 15:08:48 -0500

    chore(sddm): stop compiling weston from scratch
    will firewall block the VNC support instead

--- modules/sddm.nix
+++ modules/sddm.nix
@@ -16,11 +16,5 @@
         extraPackages = [ sddm-theme ];
         settings.General.GreeterEnvironment = "QT_SCREEN_SCALE_FACTORS=${toString config.dots.display.scale}";
       };
-
-      nixpkgs.overlays = [
-        (_: prev: {
-          weston = prev.weston.override { vncSupport = false; };
-        })
-      ];
     };
 }
