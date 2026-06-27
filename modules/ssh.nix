c37252fa55fc1d4fd66860627620e5e4a5b916bd
Matt Rogers <codemattr@gmail.com>
2026-06-27 15:10:12 -0500

    chore(ssh): switch to the new home manager config syntax

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -16,7 +16,7 @@
     programs.ssh = {
       enable = true;
       enableDefaultConfig = false;
-      matchBlocks."*" = {
+      settings."*" = {
         forwardAgent = false;
         addKeysToAgent = "no";
         compression = false;
