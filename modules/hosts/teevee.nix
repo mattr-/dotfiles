2b5c5737f974ef3b49d2a05fe4c22d4487774ffa
Matt Rogers <codemattr@gmail.com>
2026-07-11 17:25:21 -0500

    feat(teevee): new host config
    wip though

---
+++ modules/hosts/teevee.nix
@@ -0,0 +1,28 @@
+{ inputs, config, ... }:
+let
+  nixosModules = builtins.attrValues (config.flake.modules.nixos or { });
+  hmModules = builtins.attrValues (config.flake.modules.homeManager or { });
+in
+{
+  flake.nixosConfigurations."teevee" = inputs.nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    specialArgs = { inherit inputs; };
+    modules = nixosModules ++ [
+      inputs.hardware.nixosModules.common-cpu-intel-cpu-only
+      inputs.hardware.nixosModules.common-pc-ssd
+      inputs.disko.nixosModules.disko
+
+      ./teevee/_hardware-configuration.nix
+      ./teevee/_disko.nix
+
+      ({ pkgs, ... }: {
+        nixpkgs.hostPlatform = "x86_64-linux";
+        networking.hostName = "teevee";
+        hardware.gpu = "nvidia";
+        hardware.enableRedistributableFirmware = true;
+
+        hm.imports = hmModules;
+      })
+    ];
+  };
+}
