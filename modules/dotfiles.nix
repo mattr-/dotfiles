83bfc357cce1abf07f5f91de0e56b205051aca45
Matt Rogers <codemattr@gmail.com>
2026-07-30 16:21:10 -0500

    feat(dotfiles): add all remaining configs including legacy

--- modules/dotfiles.nix
+++ modules/dotfiles.nix
@@ -1,6 +1,6 @@
 { ... }:
 {
-  flake.modules.homeManager.dotfiles = { ... }: {
+  flake.modules.homeManager.dotfiles = { lib, pkgs, ... }: {
     home.file = {
       ".config/ghostty/config".source = ../home/dot_config/ghostty/config;
       ".config/wezterm/wezterm.lua".source = ../home/dot_config/wezterm/wezterm.lua;
@@ -70,6 +70,74 @@
       ".p10k.zsh".source = ../home/dot_p10k.zsh;
       ".tmux.conf".source = ../home/dot_tmux.conf;
       ".githelpers".source = ../home/dot_githelpers;
+
+      ".ansible.cfg".source = ../home/dot_ansible.cfg;
+      ".bash_profile".source = ../home/dot_bash_profile;
+      ".bashrc".source = ../home/dot_bashrc;
+      ".ctags".source = ../home/dot_ctags;
+      ".cvsignore".source = ../home/dot_cvsignore;
+      ".fonts.conf".source = ../home/dot_fonts.conf;
+      ".gemrc".source = ../home/dot_gemrc;
+      ".gtkrc-2.0".source = ../home/dot_gtkrc-2.0;
+      ".i3status.conf".source = ../home/dot_i3status.conf;
+      ".macos".source = ../home/dot_macos;
+      ".mrconfig".source = ../home/dot_mrconfig;
+      ".mrconfig.d/.keep".text = "";
+      ".msmtprc".source = ../home/dot_msmtprc;
+      ".muttrc".source = ../home/dot_muttrc;
+      ".my.cnf".source = ../home/dot_my.cnf;
+      ".notmuch-config".source = ../home/dot_notmuch-config;
+      ".offlineimaprc".source = ../home/dot_offlineimaprc;
+      ".pryrc".source = ../home/dot_pryrc;
+      ".psqlrc".source = ../home/dot_psqlrc;
+      ".reek".source = ../home/dot_reek;
+      ".screenrc".source = ../home/dot_screenrc;
+      ".slate".source = ../home/dot_slate;
+      ".spacemacs".source = ../home/dot_spacemacs;
+      ".todo.cfg".source = ../home/dot_todo.cfg;
+      ".tool-versions".source = ../home/dot_tool-versions;
+      ".urlview".source = ../home/dot_urlview;
+      ".vimrc".source = ../home/dot_vimrc;
+      ".Xdefaults".source = ../home/dot_Xdefaults;
+
+      ".atom/.gitignore".source = ../home/dot_atom/dot_gitignore;
+      ".atom/keymap.cson".source = ../home/dot_atom/keymap.cson;
+
+      ".bundle/config".source = ../home/dot_bundle/config;
+
+      ".emacs.d/init.el".source = ../home/dot_emacs.d/init.el;
+      ".emacs.d/mattr.el".source = ../home/dot_emacs.d/mattr.el;
+      ".emacs.d/mattr.org".source = ../home/dot_emacs.d/mattr.org;
+      ".emacs.d/lisp".source = ../home/dot_emacs.d/lisp;
+
+      ".i3/config".source = ../home/dot_i3/config;
+
+      ".mutt/view_attachment.sh" = { source = ../home/dot_mutt/executable_view_attachment.sh; executable = true; };
+      ".mutt/mailcap".source = ../home/dot_mutt/mailcap;
+      ".mutt/offlineimap.py".source = ../home/dot_mutt/offlineimap.py;
+
+      ".vim/after".source = ../home/dot_vim/after;
+      ".vim/autoload".source = ../home/dot_vim/autoload;
+      ".vim/colors".source = ../home/dot_vim/colors;
+      ".vim/ftdetect".source = ../home/dot_vim/ftdetect;
+      ".vim/snippets".source = ../home/dot_vim/snippets;
+      ".vim/backup/.keep".text = "";
+
+      ".local/share/nvim/site/markdown_preview.css".source = ../home/private_dot_local/private_share/nvim/site/markdown_preview.css;
+    } // lib.optionalAttrs pkgs.stdenv.isDarwin {
+      ".hammerspoon/init.lua".source = ../home/dot_hammerspoon/init.lua;
+      ".hammerspoon/Spoons/AutoClick.spoon/docs_index.json".source = ../home/dot_hammerspoon/Spoons/AutoClick.spoon/docs_index.json;
+      ".hammerspoon/Spoons/AutoClick.spoon/docs.json".source = ../home/dot_hammerspoon/Spoons/AutoClick.spoon/docs.json;
+      ".hammerspoon/Spoons/AutoClick.spoon/init.lua".source = ../home/dot_hammerspoon/Spoons/AutoClick.spoon/init.lua;
+      ".hammerspoon/Spoons/AutoClick.spoon/markdown/AutoClick.md".source = ../home/dot_hammerspoon/Spoons/AutoClick.spoon/markdown/AutoClick.md;
+      ".hammerspoon/Spoons/AutoClick.spoon/markdown/index.md".source = ../home/dot_hammerspoon/Spoons/AutoClick.spoon/markdown/index.md;
+      ".hammerspoon/Spoons/ControlEscape.spoon/Brewfile".source = ../home/dot_hammerspoon/Spoons/ControlEscape.spoon/executable_Brewfile;
+      ".hammerspoon/Spoons/ControlEscape.spoon/docs.json".source = ../home/dot_hammerspoon/Spoons/ControlEscape.spoon/executable_docs.json;
+      ".hammerspoon/Spoons/ControlEscape.spoon/init.lua".source = ../home/dot_hammerspoon/Spoons/ControlEscape.spoon/executable_init.lua;
+      ".hammerspoon/Spoons/ControlEscape.spoon/LICENSE".source = ../home/dot_hammerspoon/Spoons/ControlEscape.spoon/executable_LICENSE;
+      ".hammerspoon/Spoons/ControlEscape.spoon/README.md".source = ../home/dot_hammerspoon/Spoons/ControlEscape.spoon/executable_README.md;
+      ".hammerspoon/Spoons/ControlEscape.spoon/script/generate-docs".source = ../home/dot_hammerspoon/Spoons/ControlEscape.spoon/script/executable_generate-docs;
+      ".hammerspoon/Spoons/ControlEscape.spoon/script/setup" = { source = ../home/dot_hammerspoon/Spoons/ControlEscape.spoon/script/executable_setup; executable = true; };
     };
   };
 }
