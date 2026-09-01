b124aa87ff03b7aa09d03dc505c309c27d1969b2
Matt Rogers <codemattr@gmail.com>
2026-09-01 15:08:23 -0500

    chore(emacs): remove old config

--- home/dot_emacs.d/lisp/init-magit.el
+++
@@ -1,9 +0,0 @@
-(use-package magit :ensure t)
-
-(use-package evil-magit :ensure t)
-
-(bind-map-set-keys my-base-leader-map
-                   "gs" 'magit-status
-                   )
-
-(provide 'init-magit)
