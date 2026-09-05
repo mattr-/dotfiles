ee39c702be9df2cc5e37b267a7a3a1b96408caa4
Matt Rogers <codemattr@gmail.com>
2026-09-04 20:35:29 -0500

    chore(doom): better defaults
    :set sw=2 splitbelow splitright

--- home/dot_config/doom/config.el
+++ home/dot_config/doom/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-;; (setq user-full-name "John Doe"
-;;       user-mail-address "john@doe.com")
+(setq user-full-name "Matt Rogers"
+      user-mail-address "codemattr@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
@@ -73,3 +73,6 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+(setq evil-shift-width 2)
+(setq evil-split-window-below t)
+(setq evil-vsplit-window-right t)
