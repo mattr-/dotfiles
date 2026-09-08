d1e89f81d3aa13eb22fe145ead90de46ed2f770d
Matt Rogers <codemattr@gmail.com>
2026-09-08 13:54:44 -0500

    chore(doom): font and project updates

--- home/dot_config/doom/config.el
+++ home/dot_config/doom/config.el
@@ -28,6 +28,7 @@
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
 ;; wasn't installed correctly. Font issues are rarely Doom issues!
+(setq doom-font (font-spec :family "Iosevka Term" :size 12 :weight 'semi-light))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -76,3 +77,5 @@
 (setq evil-shift-width 2)
 (setq evil-split-window-below t)
 (setq evil-vsplit-window-right t)
+(with-eval-after-load 'projectile
+                      (setq projectile-project-search-path '(("~/Code" . 2) "~/.local/share/chezmoi")))
