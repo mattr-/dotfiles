ef02d6fe9d5103c1a7f58f2d45635b9a9b63759c
Matt Rogers <codemattr@gmail.com>
2026-09-08 20:21:02 -0500

    chore(niri): Use Mod+Space for launcher

--- home/dot_config/doom/config.el
+++ home/dot_config/doom/config.el
@@ -28,7 +28,7 @@
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
 ;; wasn't installed correctly. Font issues are rarely Doom issues!
-(setq doom-font (font-spec :family "Iosevka Term" :size 12 :weight 'semi-light))
+(setq doom-font (font-spec :family "Iosevka Term" :size 14 :weight 'semi-light))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
