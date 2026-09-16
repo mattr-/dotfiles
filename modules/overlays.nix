44149009db041b84fe25dff9b161efd1f43f1978
Matt Rogers <codemattr@gmail.com>
2026-09-16 14:17:19 -0500

    chore: downgrade xwayland-satellite to 0.8.1

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -1,7 +1,27 @@
 { inputs, ... }:
 let
-  localOverlay = final: _prev: {
+  localOverlay = final: prev: {
     plannotator = final.callPackage ../pkgs/plannotator { };
+
+    xwayland-satellite =
+      let
+        version = "0.8.1";
+        src = final.fetchFromGitHub {
+          owner = "Supreeeme";
+          repo = "xwayland-satellite";
+          tag = "v${version}";
+          hash = "sha256-BUE41HjLIGPjq3U8VXPjf8asH8GaMI7FYdgrIHKFMXA=";
+        };
+      in
+      prev.xwayland-satellite.overrideAttrs (_oldAttrs: {
+        inherit version src;
+
+        cargoDeps = final.rustPlatform.fetchCargoVendor {
+          pname = "xwayland-satellite";
+          inherit version src;
+          hash = "sha256-16L6gsvze+m7XCJlOA1lsPNELE3D364ef2FTdkh0rVY=";
+        };
+      });
   };
 
   overlays = [
